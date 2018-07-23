7833e9da2bcc512e8bb1e213d7e42001304a876b
deadrules <82d3e633e05a0b663b658ab8b51750ed098a6ebe>
2018-07-23 17:49:05 +0545

    send message to server sucessfully

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 window.navigator.userAgent = 'react-native';
 import io from 'socket.io-client/dist/socket.io';
 
 
 export default class App extends React.Component {
     state = {
-        name: 1,
+        count: 1,
+        message: 'a',
     }
 
     constructor() {
@@ -14,14 +15,52 @@ export default class App extends React.Component {
 
         this.socket = io('http://localhost:8000', { jsonp: false });
 
-        this.socket.on('update', () => this.setState({ name: this.state.name + 1 }))
+        this.socket.on('update', () => this.setState({ count: this.state.count + 1 }))
         // this.socket.on('update', () => this.setState(prevState => { count: prevState.count + 1 }))
+    }
+
+    message = (text) => {
+        this.setState({ message: text })
+    }
+
+    componentDidMount() {
+
+    }
+
+    sndMessage = () => {
+        const socket = io('http://localhost:8000', { jsonp: false });
+
+        socket.emit('chat message', this.state.message);
+        if (this.state.message == '') {
+            return false;
+        }
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hello, {this.state.name}</Text>
+                <View style={{ flex: 4, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>Count, {this.state.count}</Text>
+                </View>
+
+                <View style={{ flex: 1, justifyContent: 'flex-end', }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                        <View style={{ width: '75%', justifyContent: 'center' }}>
+                            <TextInput
+                                placeholder="Type Your Message"
+                                onChangeText={(text) => this.message(text)}
+                            />
+                        </View>
+
+                        <View style={{ width: '25%' }}>
+                            <Button
+                                title="send"
+                                onPress={() => this.sndMessage()}
+                            />
+                        </View>
+
+                    </View>
+                </View>
             </View>
         );
     }
@@ -31,7 +70,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+
     },
 });
--- server/app.js
+++ server/app.js
@@ -3,13 +3,24 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(8000);
+
+var PORT = 8000;
+server.listen(PORT, () => console.log('listing to PORT ', PORT));
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-    console.log(socket.io)
-    socket.on('update', () => io.emit('update'))
+    console.log('a user is connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnectd')
+    })
+    socket.on('update', () => io.emit('update'));
+
+    //chat message
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg)
+    })
 });
+
